5ecafc00b6d2e363cd773de9ebb413c0d446e4eb
Jeremiah Holden <jeremiah@cardconnectcp.com>
2020-11-17 21:21:06 -0800

    added new dynamic port

--- server.js
+++ server.js
@@ -6,7 +6,8 @@ var path = require("path");
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 3001;
+
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
@@ -89,6 +90,6 @@ app.post("/api/characters", function(req, res) {
 
 // Starts the server to begin listening
 // =============================================================
-app.listen(PORT, function() {
-  console.log("App listening on PORT " + PORT);
-});
+app.listen(PORT, function() { app.get("/", function(req, res) {
+  res.json(path.join(__dirname, "public/index.html"));
+})});
